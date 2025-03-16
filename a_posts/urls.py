97187b6d26813c341a42d9b32420a2265dc5cf33
Saksham Gupta <guptasaksham495@gmail.com>
2025-03-16 17:01:04 +0530

    Add post deletion functionality; implement post_delete_view and corresponding template; update URLs and home template for delete action

--- a_posts/urls.py
+++ a_posts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home_view, name="home_view"),
     path("create/", views.PostCreateView.as_view(), name="create_post"),
+    path("delete/<uuid:pk>/", views.post_delete_view, name="delete_post"),
 ]
--- a_posts/views.py
+++ a_posts/views.py
@@ -3,23 +3,26 @@ from .models import Post
 from .forms import PostForm
 from django.views import View
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 def home_view(request):
     posts = Post.objects.all()
-    context = {
-        'posts': posts
-    }
-    return render(request, 'a_posts/home.html', context)
+    context = {"posts": posts}
+    return render(request, "a_posts/home.html", context)
+
 
 class PostCreateView(View):
     def get(self, request):
         form = PostForm()
-        return render(request, 'a_posts/post_create.html', {'form': form})
+        return render(request, "a_posts/post_create.html", {"form": form})
 
-    def post(self, request):
-        form = PostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse_lazy('home_view'))
-        return render(request, 'a_posts/post_create.html', {'form': form})
+
+def post_delete_view(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        post.delete()
+        return redirect(reverse_lazy("home_view"))
+    context = {"post": post}
+    return render(request, "a_posts/post_delete.html", context)
